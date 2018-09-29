09d8f430beebec1f0f45c4377fe37bb65e1b38fa
chenhua <1107504318@qq.com>
2018-09-29 13:34:57 +0800

    修改helloword组件名称为index； 删除一些注释； 添加一些默认样式；

--- vueTest/src/main.js
+++ vueTest/src/main.js
@@ -1,22 +1,10 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 import '@/assets/css/style.css'
 
-//element全部引入
-// import ElementUI from 'element-ui';
-// import 'element-ui/lib/theme-chalk/index.css';
-// Vue.use(ElementUI);
-
-//element按需引入
-// import { Upload } from 'element-ui';
-// Vue.use(Upload)
-
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
